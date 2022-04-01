9edeb6eef7a989ba9c64fd598c995db31c9dc654
Felipe <89319180+FelipeMCM@users.noreply.github.com>
2022-04-01 06:22:14 -0300

    Thousandlish 2.1
    Faltam alguns detalhes para este programa que serão implementados posteriormente.

---
+++ thousandlish2.1/src/thousandlish/Traducao.java
@@ -0,0 +1,19 @@
+package thousandlish;
+
+import java.util.LinkedList;
+
+
+public class Traducao {
+	private LinkedList<String> traducao = new LinkedList<String>();
+	
+	
+	
+	
+	
+	public LinkedList<String> getTraducoes() {
+		return traducao;
+	}
+	public void setTraducao(LinkedList<String> traducao) {
+		this.traducao = traducao;
+	}
+}
